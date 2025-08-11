bd0ce5b913a69a5004e42df9056b585bf1fd5a41
Your Name <you@example.com>
2025-08-11 09:11:25 -0300

    oi 3

--- app/src/main/java/com/example/bleblebleblublubl/MainActivity.java
+++ app/src/main/java/com/example/bleblebleblublubl/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bleblebleblublubl;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    int cantador=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.tv);
+        textView.setText(Integer.toString(contador));
+        textView.setOnClickListener(v -> {
+
+            textView.setText("bla");
+
+
+
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
