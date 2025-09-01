e57f2907f3e65e5f7214d2a44c1f7979795df06d
Your Name <you@example.com>
2025-09-01 09:03:39 -0300

    sorteio pronto

--- app/src/main/java/com/example/bleblebleblublubl/MainActivity.java
+++ app/src/main/java/com/example/bleblebleblublubl/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.bleblebleblublubl;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +12,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.util.Random;
+import java.util.RandomAccess;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button; //aponta inicialmente para null
+    EditText editTextMin, editTextMax;
+    TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        button =findViewById(R.id.button);
+        editTextMin =findViewById(R.id.edMin);
+        editTextMax =findViewById(R.id.edMax);
+        tvResultado = findViewById(R.id.tvResultado);
+
+
+        button.setOnClickListener(v -> {
+            Random random = new Random();
+            int min,max;
+            min= Integer.parseInt(editTextMin.getText().toString());
+            max= Integer.parseInt(editTextMax.getText().toString());
+            int delta = max-min;
+
+
+            int sortiado = random.nextInt(delta)+min;
+            tvResultado.setText(Integer.toString(sortiado));
+
+
+
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
