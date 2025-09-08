53df389e64c1307f525c07e5209a0b3be15640d1
lia <lia.bfs@aluno.ifsc.edu.br>
2025-09-08 09:13:38 -0300

    imc

--- app/src/main/java/com/example/bleblebleblublubl/IMC_Resultado.java
+++ app/src/main/java/com/example/bleblebleblublubl/IMC_Resultado.java
@@ -1,8 +1,6 @@
 package com.example.bleblebleblublubl;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class IMC_Resultado extends AppCompatActivity {
 
-
-    Button button7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        button7.setOnClickListener(v->{
-            Intent intent = new Intent(this, IMC_Resultado.class);
-            startActivity(intent);
-
-
-        });
-
-
-
+        setContentView(R.layout.activity_imc_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
